aba1d7de31e78b5ebb3b8b6eb4237a37c9af028f
Juan Carlos Ortiz <ortizjuan2@gmail.com>
2018-06-24 18:43:09 -0500

    fix:	Fixing the way yaw angle is computed.
    Fixing the way yaw angle is computed based on compass hard iron
    distortion

--- data_test.py
+++ data_test.py
@@ -26,55 +26,62 @@ def rotZ(v, theta):
     R = np.array([[cos(theta), -sin(theta), 0], [sin(theta), cos(theta), 0], [0, 0, 1]])
     return np.dot(R, v)
 
+def norm(v):
+    return v / np.sqrt(sum(v**2))
 
-data_file_name = "../salida/JUN17/salida_compass.txt"
-data_file_name_inclined = "../salida/salida_090618-1536-inclinado.txt"
+def deg(angle):
+    return angle * 180. / PI
 
-f = open(data_file_name, 'r')
-
-accel_data = []
-gyro_data = []
-compass_data = []
-
-for line in f:
-    l = line.split(" ")
-    accel_data.append([float(l[0]), float(l[1]), float(l[2])])
-    gyro_data.append([float(l[3]), float(l[4]), float(l[5])])
-    compass_data.append([float(l[6]), float(l[7]), float(l[8])])
-
-f.close()
-
-accel = np.array(accel_data)
-gyro = np.array(gyro_data)
-compass = np.array(compass_data)
-
-# Rotate compass vector 180 deg around Y axis, and
+# Rotate compass vector 180 deg around X axis, and
 # 90 deg around Z axis to align with accelerometer.
 RXZ = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
 
-yaw = np.array([], dtype=np.float32)
+# data_file_name = "../salida/JUN17/salida_compass.txt"
+# data_file_name_inclined = "../salida/salida_090618-1536-inclinado.txt"
 
-for i in range(accel.shape[0]):
-    ac = accel[i] - ACCEL_OFF
-    cp = compass[i]
-    # Rotate compass
-    cp = np.dot(RYZ, cp)
-    pitch = np.arcsin(-ac[0] / 9.8)
-    roll = np.arctan2(ac[1], ac[2])
-    # remove tilt
-    cp = rotX(cp, -roll)
-    cp = rotY(cp, -pitch)
-    # remove Hard and Soft Iron
-    cp = (cp - COMPASS_HARD_IRON) / COMPASS_SOFT_IRON
-    yw = np.arctan2(cos(pitch) * (cp[2] * sin(roll) - cp[1] * cos(roll)), 
-                    (cp[0] + B * sin(I) * sin(pitch)))
-    yaw = np.append(yaw, yw)
+# f = open(data_file_name, 'r')
 
-# plt.plot(yaw, '-b', label="Yaw angle")
-# plt.legend()
-# plt.show()
-yaw_avg = sum(yaw)/len(yaw) * 180 / PI
-print ("yaw avg: %f") % yaw_avg
+# accel_data = []
+# gyro_data = []
+# compass_data = []
+
+# for line in f:
+#     l = line.split(" ")
+#     accel_data.append([float(l[0]), float(l[1]), float(l[2])])
+#     gyro_data.append([float(l[3]), float(l[4]), float(l[5])])
+#     compass_data.append([float(l[6]), float(l[7]), float(l[8])])
+
+# f.close()
+
+# accel = np.array(accel_data)
+# gyro = np.array(gyro_data)
+# compass = np.array(compass_data)
+
+
+
+# yaw = np.array([], dtype=np.float32)
+
+# for i in range(accel.shape[0]):
+#     ac = accel[i] - ACCEL_OFF
+#     cp = compass[i]
+#     # Rotate compass
+#     cp = np.dot(RYZ, cp)
+#     pitch = np.arcsin(-ac[0] / 9.8)
+#     roll = np.arctan2(ac[1], ac[2])
+#     # remove tilt
+#     cp = rotX(cp, -roll)
+#     cp = rotY(cp, -pitch)
+#     # remove Hard and Soft Iron
+#     cp = (cp - COMPASS_HARD_IRON) / COMPASS_SOFT_IRON
+#     yw = np.arctan2(cos(pitch) * (cp[2] * sin(roll) - cp[1] * cos(roll)), 
+#                     (cp[0] + B * sin(I) * sin(pitch)))
+#     yaw = np.append(yaw, yw)
+
+# # plt.plot(yaw, '-b', label="Yaw angle")
+# # plt.legend()
+# # plt.show()
+# yaw_avg = sum(yaw)/len(yaw) * 180 / PI
+# print ("yaw avg: %f") % yaw_avg
 
 ## ---
 compass_file = "/home/jom/Documents/datasheets/MPU9150/salida/JUN17/salida_compass.txt"
@@ -93,11 +100,7 @@ for line in f:
 
 f.close()
 
-def norm(v):
-    return v / np.sqrt(sum(v**2))
 
-def deg(angle):
-    return angle * 180. / PI
 
 accel = np.array(accel_data)
 gyro = np.array(gyro_data)
@@ -130,7 +133,7 @@ compassRotated = compassRotated - np.array([alfa, beta, 0.0])
 
 layout = Layout(
     xaxis=XAxis(
-       range=[-50, 50],
+       range=[-15, 15],
     #    showgrid=True,
     #    zeroline=True,
     #    showline=True,
@@ -142,7 +145,7 @@ layout = Layout(
     #    linewidth=2
     ),
     yaxis=YAxis(
-        range=[-50,50],
+        range=[-15,15],
         # showgrid=True,
         # zeroline=True,
         # showline=True,
